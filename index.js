ba76153076f6c11f17e09584c03011ea68a16b24
clawlynx <clawlynx77@gmail.com>
2024-10-10 10:03:43 +0530

    patch request cors fix

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Ensure all necessary methods are allowed
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], // Ensure all necessary methods are allowed
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   })
 );

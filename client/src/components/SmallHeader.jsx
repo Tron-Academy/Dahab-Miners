7726dd5b90b8e04ca3bb406675345a2d58f7e37c
clawlynx <clawlynx77@gmail.com>
2025-01-12 10:14:14 +0530

    alignment 0f navlinks in small header changed

--- client/src/components/SmallHeader.jsx
+++ client/src/components/SmallHeader.jsx
@@ -4,32 +4,32 @@ import { handleChatClick } from "../utils/whatsapp";
 
 export default function SmallHeader({ setSmallBar }) {
   return (
-    <header className="lg:hidden w-full main-bg text-white py-5">
-      <nav className="flex flex-col gap-2 items-center px-3">
+    <header className="lg:hidden w-full bg-[#000618] text-white py-5">
+      <nav className="flex flex-col gap-2 items-start px-3">
         <NavLink
           to={"/"}
-          className={"border-b w-full text-center py-2 border-[#9eede0]"}
+          className={"border-b w-full py-2 border-[#9eede0]"}
           onClick={() => setSmallBar(false)}
         >
           Home
         </NavLink>
         <NavLink
           to={"/buy"}
-          className={"border-b w-full text-center py-2 border-[#9eede0]"}
+          className={"border-b w-full py-2 border-[#9eede0]"}
           onClick={() => setSmallBar(false)}
         >
           Buy Miners
         </NavLink>
         <NavLink
           to={"/host"}
-          className={"border-b w-full text-center py-2 border-[#9eede0]"}
+          className={"border-b w-full py-2 border-[#9eede0]"}
           onClick={() => setSmallBar(false)}
         >
           Host Mining
         </NavLink>
         <NavLink
           to={"/repair"}
-          className={"border-b w-full text-center py-2 border-[#9eede0]"}
+          className={"border-b w-full py-2 border-[#9eede0]"}
           onClick={() => setSmallBar(false)}
         >
           Miner Repair

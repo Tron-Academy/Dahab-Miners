0e46f451dace26d589e608de0e0f3a4ea871c77e
clawlynx <clawlynx77@gmail.com>
2025-06-30 16:22:56 +0530

    authorisation routes completed

--- controllers/miningApp/miningAuthController.js
+++ controllers/miningApp/miningAuthController.js
@@ -46,3 +46,27 @@ export const miningLogin = async (req, res) => {
   });
   res.status(200).json({ msg: "successfully Logged in", token });
 };
+
+export const miningLogout = async (req, res) => {
+  // const token = req.headers.authorization?.split(" ")[1] || req.cookies.token;
+  // if (!token) throw new UnauthenticatedError("unable to access");
+  // const decoded = verifyJWT(token);
+  // decoded.exp = Date.now();
+  const token = jwt.sign({ userId: "logout" }, process.env.JWT_SECRET, {
+    expiresIn: "1s",
+  });
+  const tenDay = 1000 * 60 * 60 * 24 * 10;
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + tenDay),
+    secure: process.env.NODE_ENV === "production",
+  });
+  res.status(200).json({ message: "Logged out successfully ", token });
+};
+
+export const getMiningUserInfo = async (req, res) => {
+  const { userId } = req.user;
+  const user = await MiningUser.findById(userId).select("-password");
+  if (!user) throw new NotFoundError("No user found");
+  res.status(200).json(user);
+};

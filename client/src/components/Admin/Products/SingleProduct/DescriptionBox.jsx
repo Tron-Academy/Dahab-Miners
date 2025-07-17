bd50d4ad7f6b84d71f512678cddf4664c579661c
trondigital <130270321+trondigital@users.noreply.github.com>
2025-07-17 11:41:31 +0530

    Delete client directory

--- client/src/components/Admin/Products/SingleProduct/DescriptionBox.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function DescriptionBox({ desc }) {
-  return (
-    <div className="bg-btnGreen p-5 rounded-lg">
-      <p className=" leading-8">{desc}</p>
-    </div>
-  );
-}

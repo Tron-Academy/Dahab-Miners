16a083abd6f627903a4fd82725b3ec6eebe6e15d
clawlynx <clawlynx77@gmail.com>
2025-10-27 14:50:12 +0530

    payout mode switching if mining is in loss

--- cronJobs/walletDeductions.js
+++ cronJobs/walletDeductions.js
@@ -59,15 +59,33 @@ export const calculateAndDeductHostingFee = async () => {
           });
         } else if (user.payoutMode === "profit") {
           const hostingFeeInBTC = totalHostingFee / btcPriceAED;
-          user.currentBalance = (user.currentBalance || 0) - hostingFeeInBTC;
-          user.ProfitModeDeductions.push({
-            date: now,
-            amountAED: totalHostingFee,
-            amountBTC: hostingFeeInBTC,
-            rateBTCNowAED: btcPriceAED,
-            message:
-              "Hosting for owned S19KPro, S21 (if any) based on 100% uptime",
-          });
+          const lastReward =
+            user.allMinedRewards?.length > 0
+              ? user.allMinedRewards[user.allMinedRewards.length - 1]
+              : null;
+          const lastRewardAmount = lastReward?.amount || 0;
+          if (hostingFeeInBTC < lastRewardAmount) {
+            user.currentBalance = (user.currentBalance || 0) - hostingFeeInBTC;
+            user.ProfitModeDeductions.push({
+              date: now,
+              amountAED: totalHostingFee,
+              amountBTC: hostingFeeInBTC,
+              rateBTCNowAED: btcPriceAED,
+              message:
+                "Hosting for owned S19KPro, S21 (if any) based on 100% uptime",
+            });
+          } else {
+            user.payoutMode = "hold";
+            user.walletBalance = (user.walletBalance || 0) - totalHostingFee;
+            user.walletTransactions.push({
+              date: now,
+              amount: totalHostingFee,
+              type: "debited",
+              message:
+                "Hosting For owned S19KPro, S21 (if any) based on 100% uptime (Payout mode auto-switched to hold mode)",
+              currentWalletBalance: user.walletBalance,
+            });
+          }
         }
         await user.save({ session });
       }
@@ -81,48 +99,3 @@ export const calculateAndDeductHostingFee = async () => {
     console.log("something went wrong", error.message);
   }
 };
-
-// export const calculateAndDeductHostingFee = async () => {
-//   const session = await mongoose.startSession();
-//   session.startTransaction();
-//   try {
-//     const users = await MiningUser.find({
-//       "ownedMiners.0": { $exists: true },
-//     })
-//       .populate("ownedMiners.itemId")
-//       .session(session);
-//     const now = new Date();
-//     const endOfDay = new Date(now);
-//     endOfDay.setHours(23, 59, 59, 999);
-//     for (let user of users) {
-//       let totalHostingFee = 0;
-//       if (user.payoutMode === "profit") continue;
-//       for (const owned of user.ownedMiners) {
-//         const product = owned.itemId;
-//         if (owned.validity && new Date(owned.validity) < endOfDay) continue;
-//         if (!product || !product.power || !product.hostingFeePerKw) continue;
-//         const fee =
-//           owned.qty * product.power * 24 * product.hostingFeePerKw * 3.67 * 0.9;
-//         totalHostingFee += fee;
-//         owned.hostingFeePaid = (owned.hostingFeePaid || 0) + fee;
-//       }
-//       if (totalHostingFee > 0) {
-//         user.walletBalance = (user.walletBalance || 0) - totalHostingFee;
-//         user.walletTransactions.push({
-//           date: now,
-//           amount: totalHostingFee,
-//           type: "debited",
-//           currentWalletBalance: user.walletBalance,
-//         });
-//         await user.save({ session });
-//       }
-//     }
-//     await session.commitTransaction();
-//     session.endSession();
-//     console.log("Hosting Fees deducted successfully");
-//   } catch (error) {
-//     await session.abortTransaction();
-//     session.endSession();
-//     console.log("something went wrong", error.message);
-//   }
-// };

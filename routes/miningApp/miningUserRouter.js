e8ed82fdf09bfcfcae321b6717f8eaa68e5e3b01
clawlynx <clawlynx77@gmail.com>
2025-12-05 07:35:13 +0530

    added a route for owned miners

--- routes/miningApp/miningUserRouter.js
+++ routes/miningApp/miningUserRouter.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   getAllMiningUsers,
+  getUsersMiners,
   updateWalletBalance,
 } from "../../controllers/miningApp/miningUserController.js";
 import { validateUpdateUserWallet } from "../../middleware/validationMiddleware.js";
@@ -9,5 +10,6 @@ const router = Router();
 
 router.get("/", getAllMiningUsers);
 router.patch("/wallet", validateUpdateUserWallet, updateWalletBalance);
+router.get("/owned", getUsersMiners);
 
 export default router;

9903799a32e9a164e0d6991bdaffd7d30f785f3c
clawlynx <clawlynx77@gmail.com>
2025-12-17 19:37:57 +0530

    added event management full code

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,6 +1,8 @@
 import { formatImage } from "../middleware/multerMiddleware.js";
 import { v2 as cloudinary } from "cloudinary";
 import Event from "../models/Events.js";
+import { NotFoundError } from "../errors/customErrors.js";
+import { cleanupCloudinaryImages } from "../utils/cloudinaryFunctions.js";
 
 export const addNewEvent = async (req, res) => {
   let uploadedPublicIds = [];
@@ -67,6 +69,7 @@ export const addNewEvent = async (req, res) => {
     await newEvent.save();
     res.status(200).json({ message: "Event added", newEvent });
   } catch (error) {
+    await cleanupCloudinaryImages(uploadedPublicIds);
     res.status(500).json({ msg: error.msg || error.message });
   }
 };
@@ -75,6 +78,156 @@ export const getAllEvents = async (req, res) => {
   try {
     const events = await Event.find().sort({ createdAt: -1 });
     res.status(200).json(events);
+  } catch (error) {
+    res.status(500).json({ msg: error.msg || error.message });
+  }
+};
+
+export const getSingleEvent = async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
+    if (!event) throw new NotFoundError("No event found");
+    res.status(200).json(event);
+  } catch (error) {
+    res.status(500).json({ msg: error.msg || error.message });
+  }
+};
+
+export const deleteImage = async (req, res) => {
+  try {
+    const { publicId, eventId, imageType } = req.body;
+    const event = await Event.findById(eventId);
+    if (!event) throw new NotFoundError("No event found");
+    if (imageType === "main") {
+      await cloudinary.uploader.destroy(publicId);
+      event.mainImage = null;
+      await event.save();
+      return res.status(200).json({ msg: "Image deleted successfully" });
+    } else if (imageType === "small") {
+      await cloudinary.uploader.destroy(publicId);
+      event.smallImage = null;
+      await event.save();
+      return res.status(200).json({ msg: "Image deleted successfully" });
+    } else if (imageType === "extra") {
+      await cloudinary.uploader.destroy(publicId);
+      event.extraImage = null;
+      await event.save();
+      return res.status(200).json({ msg: "Image deleted successfully" });
+    } else if (imageType === "carousel") {
+      await cloudinary.uploader.destroy(publicId);
+      event.carouselImages = event.carouselImages.filter(
+        (item) => item.publicId !== publicId
+      );
+      await event.save();
+      return res.status(200).json({ msg: "Image deleted successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: error.msg || error.message });
+  }
+};
+
+export const editEvent = async (req, res) => {
+  let uploadedPublicIds = [];
+  try {
+    const {
+      title,
+      date,
+      mainContent,
+      bottomContent,
+      location,
+      slug,
+      altText,
+      hostedBy,
+      metaTitle,
+      metaDescription,
+      metaKeywords,
+    } = req.body;
+    const event = await Event.findById(req.params.id);
+    if (!event) throw new NotFoundError("No event found");
+    const uploadSingle = async (file) => {
+      const formatted = formatImage(file);
+      const res = await cloudinary.uploader.upload(formatted);
+      uploadedPublicIds.push(res.public_id);
+      return {
+        url: res.secure_url,
+        publicId: res.public_id,
+      };
+    };
+    const data = {
+      smallImage: event.smallImage || null,
+      mainImage: event.mainImage || null,
+      extraImage: event.extraImage || null,
+      carouselImages: event.carouselImages || [],
+    };
+    if (req.files?.mainImage?.[0]) {
+      if (data.mainImage?.publicId) {
+        await cloudinary.uploader.destroy(data.mainImage.publicId);
+      }
+      data.mainImage = await uploadSingle(req.files.mainImage[0]);
+    }
+    if (req.files?.smallImage?.[0]) {
+      if (data.smallImage?.publicId) {
+        await cloudinary.uploader.destroy(data.smallImage.publicId);
+      }
+      data.smallImage = await uploadSingle(req.files.smallImage[0]);
+    }
+    if (req.files?.extraImage?.[0]) {
+      if (data.extraImage?.publicId) {
+        await cloudinary.uploader.destroy(data.extraImage.publicId);
+      }
+      data.extraImage = await uploadSingle(req.files.extraImage[0]);
+    }
+    if (req.files?.carouselImages?.length > 0) {
+      const newImages = await Promise.all(
+        req.files.carouselImages.map((file) => uploadSingle(file))
+      );
+      data.carouselImages = [...data.carouselImages, ...newImages];
+    }
+    event.title = title;
+    event.date = new Date(date);
+    event.location = location;
+    event.mainContent = mainContent;
+    event.bottomContent = bottomContent || "";
+    event.slug = slug;
+    event.metaTitle = metaTitle;
+    event.metaDescription = metaDescription;
+    event.metaKeywords = metaKeywords;
+    event.altText = altText;
+    event.hostedBy = hostedBy || "";
+    event.mainImage = data.mainImage;
+    event.smallImage = data.smallImage;
+    event.extraImage = data.extraImage;
+    event.carouselImages = data.carouselImages;
+    await event.save();
+    res.status(200).json({ msg: "Updated successfully" });
+  } catch (error) {
+    await cleanupCloudinaryImages(uploadedPublicIds);
+    res.status(500).json({ msg: error.msg || error.message });
+  }
+};
+
+export const deleteEvent = async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
+    if (!event) throw new NotFoundError("No event found");
+    if (event.mainImage?.publicId) {
+      await cloudinary.uploader.destroy(event.mainImage.publicId);
+    }
+    if (event.smallImage?.publicId) {
+      await cloudinary.uploader.destroy(event.smallImage.publicId);
+    }
+    if (event.extraImage?.publicId) {
+      await cloudinary.uploader.destroy(event.extraImage.publicId);
+    }
+    if (event.carouselImages?.length > 0) {
+      await Promise.all(
+        event.carouselImages.map((item) =>
+          cloudinary.uploader.destroy(item.publicId)
+        )
+      );
+    }
+    await event.deleteOne();
+    res.status(200).json({ msg: "event deleted" });
   } catch (error) {
     res.status(500).json({ msg: error.msg || error.message });
   }
--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
 import { authenticateUser, isAdmin } from "../middleware/authMiddleware.js";
 import upload from "../middleware/multerMiddleware.js";
-import { addNewEvent, getAllEvents } from "../controllers/eventController.js";
-import { validateAddEvent } from "../middleware/validationMiddleware.js";
+import {
+  addNewEvent,
+  deleteEvent,
+  deleteImage,
+  editEvent,
+  getAllEvents,
+  getSingleEvent,
+} from "../controllers/eventController.js";
+import {
+  validateAddEvent,
+  validateDeleteImage,
+} from "../middleware/validationMiddleware.js";
 
 const router = Router();
 
@@ -20,5 +30,28 @@ router.post(
   addNewEvent
 );
 router.get("/", getAllEvents);
+router.get("/:id", getSingleEvent);
+
+router.patch(
+  "/delete-image",
+  authenticateUser,
+  isAdmin,
+  validateDeleteImage,
+  deleteImage
+);
+router.patch(
+  "/:id",
+  authenticateUser,
+  isAdmin,
+  upload.fields([
+    { name: "mainImage", maxCount: 1 },
+    { name: "smallImage", maxCount: 1 },
+    { name: "extraImage", maxCount: 1 },
+    { name: "carouselImages", maxCount: 7 },
+  ]),
+  validateAddEvent,
+  editEvent
+);
+router.delete("/:id", authenticateUser, isAdmin, deleteEvent);
 
 export default router;
